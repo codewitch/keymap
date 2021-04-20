88a3ec7c0c42b6019a0a5d39f8fbc70471323903
Tommy <tommyjeon@tommys-mbp.lan>
2021-04-19 22:41:59 -0700

    OLED configurations

--- keyboards/lily58/lib/layer_state_reader.c
+++ keyboards/lily58/lib/layer_state_reader.c
@@ -4,10 +4,10 @@
 #include "lily58.h"
 
 #define L_BASE 0
-#define L_LOWER (1 << 1)
-#define L_RAISE (1 << 2)
+#define L_ONE (1 << 1)
+#define L_TWO (1 << 2)
 #define L_ADJUST (1 << 3)
-#define L_ADJUST_TRI (L_ADJUST | L_RAISE | L_LOWER)
+#define L_ADJUST_TRI (L_ADJUST | L_ONE | L_TWO)
 
 char layer_state_str[24];
 
@@ -15,13 +15,13 @@ const char *read_layer_state(void) {
   switch (layer_state)
   {
   case L_BASE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Default");
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Base");
     break;
-  case L_RAISE:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Raise");
+  case L_ONE:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: 1");
     break;
-  case L_LOWER:
-    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: Lower");
+  case L_TWO:
+    snprintf(layer_state_str, sizeof(layer_state_str), "Layer: 2");
     break;
   case L_ADJUST:
   case L_ADJUST_TRI:
